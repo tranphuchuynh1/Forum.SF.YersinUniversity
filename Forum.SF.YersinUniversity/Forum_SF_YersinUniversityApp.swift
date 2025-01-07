ec535f860c85292bc562fb16065d498a4b093c96
tranphuchuynh1 <tranphuchuynh1@gmail.com>
2025-01-07 23:27:15 +0700

    Initial Commit

---
+++ Forum.SF.YersinUniversity/Forum_SF_YersinUniversityApp.swift
@@ -0,0 +1,17 @@
+//
+//  Forum_SF_YersinUniversityApp.swift
+//  Forum.SF.YersinUniversity
+//
+//  Created by Huynh Trần on 1/7/25.
+//
+
+import SwiftUI
+
+@main
+struct Forum_SF_YersinUniversityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
